fd0ebbb322f42c59b0def3bb9ea53748314dad05
alpertunga-bile <kingdom19online@hotmail.com>
2023-09-29 21:43:27 +0300

    clears are added

--- install.sh
+++ install.sh
@@ -5,13 +5,19 @@ echo "Starting setup"
 echo "Saving yay config"
 yay --save --answerdiff None --answerclean None --removemake --devel --cleanafter
 
+clear
+
 echo "Update environment"
 yay
+
+clear
 
 # Download packages
 while read -r package; do
 yay -S --needed --noconfirm $package
 done < packages.txt
+
+clear
 
 echo "Copying config folders"
 # Copy to config folder
@@ -19,5 +25,7 @@ cp -r ./dunst/ ~/.config/
 cp -r ./hypr/ ~/.confg/
 cp -r ./swaylock ~/.config/
 cp -r ./waybar/ ~/.config/
+
+clear
 
 echo "Setup is completed"
