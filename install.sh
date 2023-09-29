2389c8b94585d592dbc5ad5e6c730283bed569c9
alpertunga-bile <kingdom19online@hotmail.com>
2023-09-29 21:33:19 +0300

    changed from python to bash

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Starting setup"
+
+echo "Saving yay config"
+yay --save --answerdiff None --answerclean None --removemake --devel --cleanafter
+
+# Download packages
+while read -r package; do
+yay -S --needed --noconfirm $package
+done < packages.txt
+
+echo "Copying config folders"
+# Copy to config folder
+cp -r ./dunst/ ~/.config/
+cp -r ./hypr/ ~/.confg/
+cp -r ./swaylock ~/.config/
+cp -r ./waybar/ ~/.config/
+
+echo "Setup is completed"
