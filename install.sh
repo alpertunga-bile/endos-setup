2c317671b915db323714c8ccb559cfa51155e3dd
alpertunga-bile <kingdom19online@hotmail.com>
2024-05-30 21:52:12 +0300

    packages are updated

--- install.sh
+++ install.sh
@@ -35,7 +35,7 @@ yay -S --needed --noconfirm otf-latinmodern-math
 # icons and themes
 yay -S --needed --noconfirm nwg-look-bin
 yay -S --needed --noconfirm sddm-conf-git
-# yay -S --needed --noconfirm qt5ct
+yay -S --needed --noconfirm qt5ct
 yay -S --needed --noconfirm nordic-theme
 yay -S --needed --noconfirm papirus-icon-theme
 yay -S --needed --noconfirm breeze-default-cursor-theme
@@ -48,9 +48,10 @@ yay -S --needed --noconfirm wireplumber
 yay -S --needed --noconfirm polkit-kde-agent
 yay -S --needed --noconfirm qt5-wayland
 yay -S --needed --noconfirm qt6-wayland
-yay -S --needed --noconfirm waybar
-yay -S --needed --noconfirm cava
+yay -S --needed --noconfirm waybar-cava-git
+yay -S --needed --noconfirm cava 
 yay -S --needed --noconfirm starship
+yay -S --needed --noconfirm eww
 yay -S --needed --noconfirm swww
 yay -S --needed --noconfirm rofi-lbonn-wayland-git
 yay -S --needed --noconfirm kitty-git
@@ -59,7 +60,10 @@ yay -S --needed --noconfirm cliphist
 yay -S --needed --noconfirm wlsunset
 yay -S --needed --noconfirm udiskie
 yay -S --needed --noconfirm xdg-desktop-portal-hyprland
+yay -S --needed --noconfirm hyprwayland-scanner-git
+yay -S --needed --noconfirm hyprcursor-git
 yay -S --needed --noconfirm hyprland-git
+yay -S --needed --noconfirm libva-nvidia-driver-git
 
 # mount
 yay -S --needed --noconfirm gvfs
@@ -79,19 +83,20 @@ yay -S --needed --noconfirm discord
 yay -S --needed --noconfirm okular
 yay -S --needed --noconfirm kcalc
 yay -S --needed --noconfirm gwenview
+yay -S --needed --noconfirm lutris 
+yay -S --needed --noconfirm oh-my-zsh-git 
+yay -S --needed --noconfirm gameconqueror 
+yay -S --needed --noconfirm unzip 
+yay -S --needed --noconfirm zsh 
 
 # coding applications
 yay -S --needed --noconfirm visual-studio-code-bin
 yay -S --needed --noconfirm ghidra
 yay -S --needed --noconfirm renderdoc
 
-# doom emacs
-yay -S --needed --noconfirm emacs
-yay -S --needed --noconfirm ripgrep
-yay -S --needed --noconfirm fd
-
 # GPGPU
 yay -S --needed --noconfirm cuda
+yay -S --needed --noconfirm optix
 
 # game engine
 yay -S --needed --noconfirm vulkan-devel
@@ -106,11 +111,11 @@ yay -S --needed --noconfirm draco
 yay -S --needed --noconfirm compressonator-git
 yay -S --needed --noconfirm meshoptimizer
 yay -S --needed --noconfirm glfw-wayland
-yay -S --needed --noconfirm meshlab
 yay -S --needed --noconfirm glm
 yay -S --needed --noconfirm cgal-git
 yay -S --needed --noconfirm embree
 yay -S --needed --noconfirm openshadinglanguage
+yay -S --needed --noconfirm ispc
 yay -S --needed --noconfirm usd
 yay -S --needed --noconfirm usd-draco
 yay -S --needed --noconfirm usd-openshadinglanguage
@@ -118,7 +123,9 @@ yay -S --needed --noconfirm usd-alembic
 yay -S --needed --noconfirm usd-openvdb
 
 # coding packages
+yay -S --needed --noconfirm angelscript 
 yay -S --needed --noconfirm cpp-taskflow
+yay -S --needed --noconfirm onetbb 
 yay -S --needed --noconfirm concurrent-queue-git
 yay -S --needed --noconfirm moonray
 yay -S --needed --noconfirm moonray-gui
@@ -129,6 +136,7 @@ clear
 echo "Copying config folders"
 cp -r ./config/* ~/.config
 cp -r ./rofi/local/share/rofi/themes/ ~/.local/share/
+cp ./zshrc ~/
 
 clear
 
