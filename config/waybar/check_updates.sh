89dfd462050822415330e5418d8d33470cac6daa
alpertunga-bile <kingdom19online@hotmail.com>
2024-06-16 14:03:57 +0300

    spotify and check_update is updated

--- config/waybar/check_updates.sh
+++ config/waybar/check_updates.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 PACMAN_NUM_PACKAGES=$(checkupdates | wc -l)
+YAOURT_NUM_PACKAGES=$(yay -Qua | wc -l)
 
-NUM_PACKAGES=$PACMAN_NUM_PACKAGES
+NUM_PACKAGES=$((PACMAN_NUM_PACKAGES + $YAOURT_NUM_PACKAGES))
 
 case $NUM_PACKAGES in
 	0)
-		echo "No packages are available"
+		echo "0 packages"
 		;;
 	1)
-		echo "1 package is available"
+		echo "1 package"
 		;;
 	*)
-		echo "$NUM_PACKAGES packages are available"
+		echo "$NUM_PACKAGES packages"
 		;;
 esac
