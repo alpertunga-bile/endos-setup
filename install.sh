466506813361efce4e6c2d635bae1c91c5d2fe53
alpertunga-bile <holy_celestine@hotmail.com>
2025-06-14 18:07:06 +0300

    install script is updated

--- install.sh
+++ install.sh
@@ -1,68 +1,97 @@
 #!/bin/bash
 
-echo "/_\\ Starting setup"
-
-echo "/_\\ Saving yay config"
 yay --save --answerdiff None --answerclean None --removemake --cleanafter
-
-echo "/_\\ Cleaning packages"
-yay --clean
 yay -Yc
-clear
 
-echo "/_\\ Updating environment"
 yay
 yay --devel
+mkdir ~/screenshots
 clear
 
-mkdir ~/screenshots
+# Packages from pkgs\essentials
+yay -S --needed cmake
+yay -S --needed ninja
+yay -S --needed clang
+yay -S --needed github-cli
+clear
 
-install_packages_from_file() {
-    local filepath="pkgs/$1"
-    echo "/_\\ Installing $1 packages"
+# Packages from pkgs\fonts
+yay -S --needed ttf-anonymous-pro
+yay -S --needed ttf-material-symbols-git
+yay -S --needed ttf-roboto-mono
+yay -S --needed ttf-jetbrains-mono
+yay -S --needed ttf-font-awesome
+yay -S --needed otf-font-awesome
+yay -S --needed ttf-monaco
+yay -S --needed ttf-hack
+yay -S --needed adobe-source-code-pro-fonts
+yay -S --needed otf-latinmodern-math
+yay -S --needed ttf-nerd-fonts-symbols
+clear
 
-    while IFS= read -r line || [[ -n "$line" ]]; do
-        yay -S --needed "$line"
-    done < "$filepath"
+# Packages from pkgs\themes
+yay -S --needed nwg-look
+yay -S --needed qt5ct
+yay -S --needed qt6ct
+yay -S --needed nordic-theme
+yay -S --needed papirus-icon-theme
+yay -S --needed breeze-default-cursor-theme
+clear
 
-    clear
+# Packages from pkgs\desktop
+yay -S --needed sddm
+yay -S --needed qt5-quickcontrols2
+yay -S --needed qt5-graphicaleffects
+yay -S --needed dunst
+yay -S --needed pipewire
+yay -S --needed wireplumber
+yay -S --needed polkit-kde-agent
+yay -S --needed qt5-wayland
+yay -S --needed qt6-wayland
+yay -S --needed waybar
+yay -S --needed starship
+yay -S --needed swww
+yay -S --needed rofi-wayland
+yay -S --needed kitty
+yay -S --needed thunar
+yay -S --needed thunar-archive-plugin
+yay -S --needed thunar-volman
+yay -S --needed cliphist
+yay -S --needed wlsunset
+yay -S --needed udiskie
+yay -S --needed xdg-desktop-portal-hyprland
+yay -S --needed hyprland
+yay -S --needed libva-nvidia-driver
+yay -S --needed grim
+yay -S --needed slurp
+yay -S --needed tumbler
+yay -S --needed ffmpegthumbnailer
+yay -S --needed hyprlock
+clear
 
-    echo "Installing $1 is finished"
-
-    sleep 3s
-}
-
-install_packages() {
-    echo "/_\\ Installing packages"
-
-    install_packages_from_file essentials
-    install_packages_from_file coding_apps
-    install_packages_from_file casual_apps
-    install_packages_from_file fonts
-    install_packages_from_file themes
-    install_packages_from_file desktop
-
-    clear
-}
-
-install_packages
+# Packages from pkgs\casual_apps
+yay -S --needed spotify
+yay -S --needed mullvad-vpn-bin
+yay -S --needed mullvad-browser-bin
+yay -S --needed discord
+yay -S --needed okular
+yay -S --needed kcalc
+yay -S --needed gwenview
+yay -S --needed zsh
+yay -S --needed zsh-syntax-highlighting
+yay -S --needed zsh-autosuggestions
+yay -S --needed p7zip
+yay -S --needed bottles
+yay -S --needed neovim
+clear
 
 systemctl enable sddm.service
 
-# Installing oh-my-zsh
-(sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)")
-
-# Copy to config folder
-echo "/_\\ Copying config folders"
-cp -r ./config/* ~/.config
-cp -r ./etc/* /etc/
-cp -r ./sddm/ /usr/share/
+# Copy configs to folders
+cp -r ./config/* ~/.config/
+sudo cp -r ./etc/* /etc/
+sudo cp -r ./sddm/ /usr/share/
 cp ./.zshrc ~/
-
 clear
 
-echo "/_\\ Setup is completed"
-
-sleep 10s
-
-reboot
+echo "/_\ Setup is completed"
